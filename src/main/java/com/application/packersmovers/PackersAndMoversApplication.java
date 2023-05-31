d25106d66e2a27246d34e22b71636308b0d723c2
unknown <rajvigneshkrishna@gmail.com>
2023-05-31 09:36:05 +0530

    Inital Commit - project setup

---
+++ src/main/java/com/application/packersmovers/PackersAndMoversApplication.java
@@ -0,0 +1,13 @@
+package com.application.packersmovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackersAndMoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackersAndMoversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/packersmovers/PackersAndMoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.packersmovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackersAndMoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
