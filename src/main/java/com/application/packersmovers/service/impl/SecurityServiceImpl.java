768f40c3ab84f90b2a60c06580193e6aa453f9e0
Rajvignesh Krishna <rajvigneshkrishna@gmail.com>
2023-06-26 21:32:56 +0530

    Security layer modified

---
+++ src/main/java/com/application/packersmovers/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,28 @@
+package com.application.packersmovers.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.application.packersmovers.model.MyUserDetails;
+import com.application.packersmovers.model.User;
+import com.application.packersmovers.repository.UserRepository;
+
+@Service
+public class SecurityServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<User> userOpt = userRepository.findByEmail(email);
+		userOpt.orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
+		return userOpt.map(MyUserDetails::new).get();
+	}
+
+}
